8b086bfd45d74585b83a7e2f56105afa1e31c085
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2018-10-05 14:26:42 -0300

    Initial commit

---
+++ Assets/SpawnObj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObj : MonoBehaviour {
+
+	public GameObject[] tiles;
+
+	
+	void Start () {
+		int randomBlock = Random.Range(0,tiles.Length);	
+		Instantiate(tiles[randomBlock], transform.position, Quaternion.identity);
+	}
+	
+	
+}
